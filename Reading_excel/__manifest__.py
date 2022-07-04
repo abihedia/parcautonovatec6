2601d6c80418d4216314b600632b94be06e5327d
abihedia <51483423+abihedia@users.noreply.github.com>
2022-07-04 12:15:26 +0100

    Add files via upload

---
+++ Reading_excel/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "ReadingExcel",
+
+    'summary': """ """,
+
+    'description': """ """,
+
+    'author': "My Company",
+    'website': " ",
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','fleet','sale'],
+    #'external_dependencies': {'python': ['StringIO'],},
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/compteurColeur.xml',
+        'views/copteurNoir.xml',
+        'views/vehicule_inherit.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
