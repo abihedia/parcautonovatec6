6156a4990e5690ae72f01525174833f5ed12ab74
abihedia <51483423+abihedia@users.noreply.github.com>
2022-07-15 10:15:36 +0100

    Update partner_invoice_inherit.py

--- partner_invoice/model/partner_invoice_inherit.py
+++ partner_invoice/model/partner_invoice_inherit.py
@@ -4,7 +4,7 @@ from odoo.exceptions import ValidationError
 class PartnerInvoiceHerit(models.Model):
     _inherit = 'res.partner'
 
-    pfr = fields.Monetary('PFR')
+    pfr = fields.Monetary('PFR', default=20)
     code_service = fields.Char('Code service')
     augmentation_sav = fields.Float(string='Augmentation SAV', default=0.05)
     augmentation_sav_bool= fields.Boolean(string="Augmentation SAV", default=True)
